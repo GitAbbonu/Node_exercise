d1c5292d3e9132834b4faed373ec4de89d1d46ac
Cipriano Mereu <ciprianomereu@gmail.com>
2023-04-07 13:26:49 +0200

    add:exercise 04

--- export.mjs
+++
@@ -1,5 +0,0 @@
-// ECMA Create a script that uses export default to export a function. Create another script that uses import to import the function and then calls it.
-
-export default function fun() {
-  console.log("export");
-}
--- require.mjs
+++
@@ -1,5 +0,0 @@
-// Create a script that uses export default to export a function. Create another script that uses import to import the function and then calls it.
-
-import fun from "./export.mjs";
-
-fun();
