b9801acafd2afd89ff36c459280cacae836b2f2e
Cipriano Mereu <ciprianomereu@gmail.com>
2023-04-24 18:09:06 +0200

    add:exercise 14

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -1,70 +1,91 @@
 import { Request, Response } from "express";
 import Joi from "joi";
 
-type Planet = {
-  id: number;
-  name: string;
+import pgPromise from "pg-promise";
+//postgres://username:password
+const db = pgPromise()("postgres://postgres:root@localhost:5432/postgres");
+
+const setupDB = async () => {
+  await db.none(`
+    DROP TABLE IF EXISTS planets;
+
+    CREATE TABLE planets (
+      id SERIAL NOT NULL PRIMARY KEY,
+      name TEXT NOT NULL,
+      image TEXT
+    )
+  `);
+
+  await db.none(`INSERT INTO planets (name) VALUES ('Earth')`);
+  await db.none(`INSERT INTO planets (name) VALUES ('Mars')`);
 };
 
-type Planets = Planet[];
+setupDB();
 
-let planets: Planets = [
-  {
-    id: 1,
-    name: "Earth",
-  },
-  {
-    id: 2,
-    name: "Mars",
-  },
-];
-
-//CRUD:
-function getAll(req: Request, res: Response) {
+//CRUD
+async function getAll(req: Request, res: Response) {
+  const planets = await db.many(`SELECT * FROM planets;`);
   res.status(200).json(planets);
 }
 
-function getOneByID(req: Request, res: Response) {
+async function getByID(req: Request, res: Response) {
   const { id } = req.params;
 
-  const planet = planets.find((p) => p.id === Number(id));
+  const planet = await db.oneOrNone(
+    `SELECT * FROM planets WHERE id=$1;`,
+    Number(id)
+  );
 
   res.status(200).json(planet);
 }
 
-const schemaPlanet = Joi.object({
-  id: Joi.number().integer().required(),
+const planetSchemaBody = Joi.object({
   name: Joi.string().required(),
 });
 
-function createP(req: Request, res: Response) {
-  const { id, name }: Planet = req.body;
+async function create(req: Request, res: Response) {
+  const { name } = req.body;
 
-  const newPlanet: Planet = { id, name };
+  const newPlanet = { name };
 
-  const valid = schemaPlanet.validate(newPlanet);
+  const validation = planetSchemaBody.validate(newPlanet);
 
-  if (valid.error) {
-    res.status(400).json("Errore");
+  if (validation.error) {
+    res.status(400).json({ msg: "error 400" });
   } else {
-    planets = [...planets, newPlanet];
-    res.status(201).json({ msg: "Pianeta creato" });
+    await db.none(`INSERT INTO planets (name) VALUES ($1)`, name);
+
+    res.status(200).json({ msg: "create new Planet" });
   }
 }
-function updateP(req: Request, res: Response) {
+
+async function updateByID(req: Request, res: Response) {
   const { id } = req.params;
   const { name } = req.body;
 
-  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
+  await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name]);
 
-  res.status(200).json({ msg: "update" });
+  res.status(200).json({ msg: "updato" });
 }
-function deleteP(req: Request, res: Response) {
+
+async function deleteByID(req: Request, res: Response) {
   const { id } = req.params;
 
-  planets = planets.filter((p) => p.id !== Number(id));
+  await db.none(`DELETE FROM planets WHERE id=$1`, Number(id));
 
-  res.status(200).json({ msg: "eliminato" });
+  res.status(200).json({ msg: "The planet was deleted." });
 }
 
-export { getAll, getOneByID, createP, updateP, deleteP };
+async function createImg(req: Request, res: Response) {
+  const { id } = req.params;
+  const fileName = req.file?.path;
+
+  if (fileName) {
+    db.none(`UPDATE planets SET image=$2 WHERE id=$1`, [id, fileName]);
+    res.status(201).json({ msg: "image uploads" });
+  } else {
+    res.status(400).json({ msg: "image failed uploads" });
+  }
+}
+
+export { getAll, getByID, create, updateByID, deleteByID, createImg };
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,57 @@
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
+import multer from "multer";
+
 import {
   getAll,
-  getOneByID,
-  createP,
-  updateP,
-  deleteP,
-} from "./controllers/planets";
+  getByID,
+  create,
+  updateByID,
+  deleteByID,
+  createImg,
+} from "./controllers/planets.js";
 
 const server = express();
-const port = 3000;
+const port = 5000;
 
 server.use(morgan("dev"));
 server.use(express.json());
 
-server.listen(port);
+server.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader("Access-Control-Allow-Credentials");
+
+  // Pass to next layer of middleware
+  next();
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 server.get("/", (req, res) => {
   res.status(200).json({ msg: "Welcome" });
@@ -23,10 +59,14 @@ server.get("/", (req, res) => {
 
 server.get("/planets", getAll);
 
-server.get("/planets/:id", getOneByID);
+server.get("/planets/:id", getByID);
 
-server.post("/planets", createP);
+server.post("/planets", create);
 
-server.put("/planets/:id", updateP);
+server.put("/planets/:id", updateByID);
 
-server.delete("/planets/:id", deleteP);
+server.delete("/planets/:id", deleteByID);
+
+server.post("/planets/:id/image", upload.single("image"), createImg);
+
+server.listen(port);
